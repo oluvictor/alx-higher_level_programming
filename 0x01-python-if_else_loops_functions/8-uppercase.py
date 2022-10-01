e22b5eeb7e8bc154250a45cff57ec0e30a12bd4e
Victor Ogunshola <vogunshola@yahoo.com>
2022-10-01 05:40:06 -0700

    fixed uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,8 +7,6 @@ def uppercase(str):
     for c in str:
         c_ascii = ord(c)
         if c_ascii >= 97 and c_ascii <= 122:
-            new_str = new_str + chr(c_ascii - 32)
-        else:
-            new_str = new_str + chr(c_ascii)
-    print(new_str, end='')
+            c = chr(c_ascii - 32)
+        print("{}".format(c), end='')
     print("")
