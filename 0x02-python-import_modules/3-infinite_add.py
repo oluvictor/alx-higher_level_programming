acdbd0ec5e591ec6da35898b8777526120257f7a
Victor Ogunshola <vogunshola@yahoo.com>
2022-10-09 15:03:27 -0700

    added task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    """prints the result of the addition of all arguments"""
+    argc = len(sys.argv) - 1
+    sum = 0
+    if argc > 0:
+        for i in range(1, argc + 1):
+            sum += int(sys.argv[i])
+    print(sum)
+
+if __name__ == "__main__":
+    """This part will only run in the terminal"""
+    main()
